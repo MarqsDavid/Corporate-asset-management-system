b27e1bf688aeb1b0a6316917fff5b83b4e9c9bf7
MarqsDavid <davidmarques.mr@gmail.com>
2022-11-15 22:08:58 -0300

    novo head

--- pages/home.php
+++ pages/home.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!----======== CSS ======== -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js">
     <!----======== CSS e BOOS ======== -->
     <link rel="stylesheet" href="./css/home.css">
